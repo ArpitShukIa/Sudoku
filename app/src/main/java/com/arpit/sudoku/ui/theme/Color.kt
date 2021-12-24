7b89a92091846685f10cece0b72584b32b3107f1
arpit <arpitshukla62@gmail.com>
2021-12-25 00:17:28 +0530

    Remove dark theme and unnecessary colors

--- app/src/main/java/com/arpit/sudoku/ui/theme/Color.kt
+++ app/src/main/java/com/arpit/sudoku/ui/theme/Color.kt
@@ -2,11 +2,6 @@ package com.arpit.sudoku.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
 val Yellow = Color(0xFFFEF6B2)
 val Blue = Color(0xFFE1EBF4)
 val LightBlue = Color(0xFF9EE5FE)
